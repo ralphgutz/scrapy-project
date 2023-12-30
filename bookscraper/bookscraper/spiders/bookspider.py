11e06ffa50bb2e2a086f637a7e2dc1caa19d8f2b
Raphael Gutierrez <ralphgutz17@gmail.com>
2023-12-31 01:50:47 +0800

    Crawl next page
    scrapy crawl bookspider

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -8,10 +8,20 @@ class BookspiderSpider(scrapy.Spider):
 
     def parse(self, response):
         books = response.css('article.product_pod')
-        
+
         for book in books:
             yield {
                 'name': book.css('h3 a::text').get(),
                 'price': book.css('.product_price .product_color::text').get(),
                 'url': book.css('h3 a').attrib['href']
             }
+
+        next_page = response.css('li.next a::attr(href)').get()
+
+        if next_page is not None:  # If element in next_page exists (next page button)
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+
+            yield response.follow(next_page_url, callback=self.parse)  # Run self.parse() to crawl next page
