a140306787e7c9f11edb488b78d58a7d4af5f801
Raphael Gutierrez <ralphgutz17@gmail.com>
2023-12-31 13:20:47 +0800

    Crawl each book webpage, export to csv
    Crawls all webpages of books per book list page
    
    Use Chrome DevTools to get XPath of an element
    
    scrapy crawl bookspider -O books.csv
    
    scrapy crawl bookspider -O books.json

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -10,11 +10,15 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css('article.product_pod')
 
         for book in books:
-            yield {
-                'name': book.css('h3 a::text').get(),
-                'price': book.css('.product_price .product_color::text').get(),
-                'url': book.css('h3 a').attrib['href']
-            }
+            relative_url = book.css('h3 a::attr(href)').get()  # Relative URL of book
+
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+
+            yield response.follow(book_url, callback=self.parse_book_page)  # Go to webpage of each book
+
 
         next_page = response.css('li.next a::attr(href)').get()
 
@@ -25,3 +29,22 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
 
             yield response.follow(next_page_url, callback=self.parse)  # Run self.parse() to crawl next page
+
+
+    def parse_book_page(self, response):  # Parse each book detail (each book has its own webpage)
+        table_row = response.css('table tr')
+
+        yield {
+            'url': response.url,
+            'title': response.css('.product_main h1::text').get(),
+            'product_type': table_row[1].css('td::text').get(),
+            'price_excl_tax': table_row[2].css('td::text').get(),
+            'price_incl_tax': table_row[3].css('td::text').get(),
+            'tax': table_row[4].css('td::text').get(),
+            'availability': table_row[5].css('td::text').get(),
+            'num_reviews': table_row[6].css('td::text').get(),
+            'stars': response.css('p.star-rating::attr(class)').get(),
+            'category': response.xpath('//*[@id="default"]/div/div/ul/li[3]/a').css('a::text').get(),  # XPath of an element (check in scrapy shell, use Chrome to copy XPath)
+            'description': response.xpath('//*[@id="content_inner"]/article/p').css('p::text').get(),
+            'price': response.css('.price_color::text').get()
+        }
